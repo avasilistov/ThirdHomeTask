f012c23acbf32c65a7b8d3adf83a4dcdd1749fc0
avasilistov <kvoroncova@gmail.com>
2021-02-24 06:13:36 +0200

    Добавил кнопку подробнее

---
+++ app/src/main/java/com/demo/thirdhometask/FragmentAdapter.java
@@ -0,0 +1,28 @@
+package com.demo.thirdhometask;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1:
+                return new SettingsFragment();
+            default:
+                return new MainFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/demo/thirdhometask/MainActivity.java
@@ -0,0 +1,74 @@
+package com.demo.thirdhometask;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.draw_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ViewPager2 pager = findViewById(R.id.pager);
+        pager.setAdapter(new FragmentAdapter(this));
+
+        TabLayout tab_layout = findViewById(R.id.tab_layout);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+                tab_layout, pager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("main");
+                        break;
+                    case 1:
+                        tab.setText("settings");
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
+}
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
+    }
+}
